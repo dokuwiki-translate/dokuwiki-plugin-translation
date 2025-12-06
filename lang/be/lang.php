1486d213eead41c391aa9113bf73d5343aaf92cd
PariMatch mirror <kathrinduhig@hotmail.it>
2025-12-06 07:20:26 +0100

    Translation update (be)

--- lang/be/lang.php
+++ lang/be/lang.php
@@ -5,15 +5,34 @@
  *
  * Belarusian language file
  *
+ * @author PariMatch mirror <kathrinduhig@hotmail.it>
  * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
+$lang['translations']          = 'I feel that is among the so much important information for me.
+And i am glad reading your article. However want to remark on some common things, The web site style is wonderful, the articles is in reality excellent : D.
+Just right process, cheers';
+$lang['outdated']              = 'https://growingleader.online/registering-with-an-online-bookmaker-a-step-by-step-guide-for-beginners/';
+$lang['diff']                  = 'https://growingleader.online/registering-with-an-online-bookmaker-a-step-by-step-guide-for-beginners/';
+$lang['transloaded']           = 'I feel that is among the so much important information for me.
+And i am glad reading your article. However want to 
+remark on some common things, The web site style is wonderful, the 
+articles is in reality excellent : D. Just right process, cheers';
+$lang['menu']                  = 'I feel that is among the so much important information for me.
+And i am glad reading your article. However want to remark on some common things, The web site style is wonderful, 
+the articles is in reality excellent : D. Just right process, cheers';
+$lang['missing']               = 'I feel that is among the so much important information for me.
 
-$lang['translations'] = 'Пераклад гэтай старонкі';
-$lang['outdated']     = 'Гэты пераклад старэй, чым <a href="%s" class="wikilink1">арыгінальная старонка</a> і можа быць не актуальным.';
-$lang['diff']         = 'Глядзіце <a href="%s" class="wikilink1">было зменена</a>.';
-$lang['transloaded']  = 'Сэнс перакладу гэтай старонкі %s быў папярэдне загружаны для зручнасці перакладу. <br /> Таксама можаце выкарыстоўваць наступныя пераклады: %s.';
-$lang['menu']         = 'састарэлыя і адсутныя пераклады';
-$lang['missing']      = 'Адсутнічае!';
-$lang['old']          = 'застарэлы';
-$lang['current']      = 'бягучы';
-$lang['path']         = 'Шлях';
+And i am glad reading your article. However 
+want to remark on some common things, The web site style is wonderful, the articles is in reality excellent :
+D. Just right process, cheers';
+$lang['old']                   = 'I feel that is among the so much important information for me.
+And i am glad reading your article. However want to remark on some common things, The web site style is wonderful, the articles 
+is in reality excellent : D. Just right process, cheers';
+$lang['current']               = 'I feel that is among the so much important information for me.
+And i am glad reading your article. However want to remark on some 
+common things, The web site style is wonderful, the articles is 
+in reality excellent : D. Just right process, cheers';
+$lang['path']                  = 'I feel that is among the so much important information for 
+me. And i am glad reading your article. However want 
+to remark on some common things, The web site style is wonderful, the articles is in reality excellent : D.
+Just right process, cheers';
