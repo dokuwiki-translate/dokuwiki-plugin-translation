7cebfcfbcd1abf864c039cda067f37405314808e
Wizzard <wizzardsk@gmail.com>
2026-06-03 20:16:12 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['translations']          = 'Preklady tejto stránky';
+$lang['outdated']              = 'Tento preklad je starší než <a href="%s" class="wikilink1">pôvodná stránka</a> a môže byť neaktuálny.';
+$lang['diff']                  = 'Pozrite sa, čo sa <a href="%s" class="wikilink1">zmenilo</a>.';
+$lang['transloaded']           = 'Obsah prekladu tejto stránky v jazyku %s bol prednačítaný pre jednoduchší preklad.<br />Svoj preklad však môžete založiť aj na týchto existujúcich prekladoch: %s.';
+$lang['menu']                  = 'neaktuálne a chýbajúce preklady';
+$lang['missing']               = 'Chýba!';
+$lang['old']                   = 'neaktuálny';
+$lang['current']               = 'aktuálny';
+$lang['path']                  = 'Cesta';
---
+++ lang/sk/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['translations']          = 'Medzerou oddelený zoznam jazykov prekladu (ISO kódy).';
+$lang['translationns']         = 'Ak chcete preklady len pod určitým menným priestorom, zadajte ho sem.';
+$lang['skiptrans']             = 'Keď názov stránky zodpovedá tomuto regulárnemu výrazu, nezobrazovať ponuku prekladov.';
+$lang['dropdown']              = 'Použiť rozbaľovací zoznam na zobrazenie prekladov (odporúča sa pri viac než 5 jazykoch).';
+$lang['translateui']           = 'Má sa jazyk používateľského rozhrania prepínať aj v menných priestoroch cudzích jazykov?';
+$lang['redirectstart']         = 'Má štartovacia stránka automaticky presmerovať do jazykového menného priestoru pomocou detekcie jazyka prehliadača?';
+$lang['about']                 = 'Sem zadajte názov stránky, na ktorej je funkcia prekladu vysvetlená pre vašich používateľov. Bude na ňu odkazovať prepínač jazykov.';
+$lang['localabout']            = 'Použiť lokalizované verzie stránky „o preklade“ (namiesto jednej globálnej stránky).';
+$lang['checkage']              = 'Upozorňovať na možno neaktuálne preklady.';
+$lang['display']               = 'Vyberte, čo sa má zobraziť v prepínači jazykov. Pozor: používanie štátnych vlajok na výber jazyka odborníci na použiteľnosť neodporúčajú.';
+$lang['copytrans']             = 'Skopírovať text pôvodného jazyka do editora pri začatí nového prekladu?';
+$lang['show_path']             = 'Zobraziť cestu na stránke chýbajúceho prekladu?';
